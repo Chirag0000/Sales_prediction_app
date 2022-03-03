2527aa5d5b75ab00c5f1e36b92f34edec03b24d7
Chirag Kataria <34574677+Chirag0000@users.noreply.github.com>
2022-03-03 20:59:14 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -11,7 +11,7 @@ LR_model=pickle.load(open('LR_model.pkl','rb'))
 # # FUNCTION
 
 def run_ml():  
-  submenu=st.sidebar.selectbox("Select the Model",["Random Forest","XgBoost","Linear Regression"])
+  submenu=st.sidebar.selectbox("Select the Model",["Random Forest","XgBoost"])
 
   if submenu=="Random Forest":
     st.subheader("Random Forest")
@@ -55,27 +55,3 @@ def run_ml():
       quantity = Xg_model.predict(report_data)
       st.subheader('Monthly Sales forecast')
       st.subheader(quantity)
-
-
-
-  elif submenu=="Linear Regression":
-    st.subheader("Linear Regression")
-    shop_id = st.slider('shop_id', 0,59, 1 )
-    item_id = st.slider('item_id', 0,22167, 1 )
-    date_block_num=st.slider('date_block_num',0,34,1)
-    Sales_per_item = st.slider('Sales_per_item', -995,2400, 1 )
-
-    data = {
-      'Shop ID':shop_id,
-      'Item ID':item_id,
-      'Date number':date_block_num,
-      'Sales for item($)':Sales_per_item,
-    }
-    report_data = pd.DataFrame(data, index=[0])
-    
-    if st.button("Predict"):
-
-      quantity = LR_model.predict(report_data)
-      st.subheader('Monthly Sales forecast')
-      st.subheader(quantity)
-
