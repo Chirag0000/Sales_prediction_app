600d8d3bc52504f5a883b61db58315b029ac3c3e
Chirag Kataria <34574677+Chirag0000@users.noreply.github.com>
2022-03-04 10:42:17 +0530

    Update ML.py

--- ML.py
+++ ML.py
@@ -58,23 +58,4 @@ def run_ml():
       
       
       
-  elif submenu=="Linear Regression":
-    st.subheader("Linear Regression")
-    shop_id = st.slider('shop_id', 0,59, 1 )
-    item_id = st.slider('item_id', 0,22167, 1 )
-    date_block_num=st.slider('date_block_num',0,34,1)
-    Sales_per_item = st.slider('Sales_per_item', -995,2400, 1 )
 
-    data = {
-      'Shop ID':shop_id,
-      'Item ID':item_id,
-      'Date number':date_block_num,
-      'Sales for item($)':Sales_per_item,
-    }
-    report_data = pd.DataFrame(data, index=[0])
-
-    if st.button("Predict"):
-
-      quantity = LR_model.predict(report_data)
-      st.subheader('Monthly Sales forecast')
-      st.subheader(quantity)
